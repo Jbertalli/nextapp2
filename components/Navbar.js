c040c329586a3192565a82be827ad57d5a089b27
Jbertalli <jjbertalli@berkeley.edu>
2022-10-31 16:09:29 -0700

    misc edits

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 // import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Menu, Dropdown, Icon, Container, Sticky, Image, Card, Feed, Grid } from 'semantic-ui-react';
+import { Menu, Dropdown, Icon, Container, Grid } from 'semantic-ui-react';
 import styles from '../styles/Footer.module.css';
 import { handleLogout } from '../utils/auth';
 // import AccountHeader from './AccountHeader';
