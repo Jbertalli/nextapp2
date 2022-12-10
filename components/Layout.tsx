168a7dd0db31e15749e1e8af114300a5c9d0b728
Jbertalli <jjbertalli@berkeley.edu>
2022-12-10 14:33:31 -0800

    night mode button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,60 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import styles from '../styles/Footer.module.css';
 import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const Layout = ({ children, user }) => {
     const router: any = useRouter();
+    const [night, setNight] = useState<boolean>(false);
+    const [background, setBackground] = useState<string>('#f2f2f2');
 
     return (
-        <div className={styles.container}>
+        <div 
+            className={styles.container} 
+            // night mode button
+            style={{ 
+                background: `${background}`
+            }}
+        >
             <Navbar user={user} />
+                {!night ? (
+                <>
+                    <Button
+                        onClick={() => {setNight(true), setBackground('#313e4c')}}
+                        style={{
+                            position: 'absolute',
+                            marginTop: '20px',
+                            marginLeft: '20px',
+                            background: 'black',
+                            color: 'white'
+                        }}
+                    >
+                        <Icon
+                            name='moon'
+                        />
+                        Night Mode
+                    </Button>
+                </>
+                ):(
+                <>
+                    <Button
+                        onClick={() => {setNight(false), setBackground('#f2f2f2')}}
+                        style={{
+                            position: 'absolute',
+                            marginTop: '20px',
+                            marginLeft: '20px',
+                            background: 'white',
+                            color: 'black'
+                        }}
+                    >
+                        <Icon
+                            name='sun'    
+                        />
+                        Day Mode
+                    </Button>
+                </>
+                )}
                 {children}
             {router.pathname !== "/404" && 
                 <div style={{ marginTop: '80px' }}>
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Divider, Input } from 'semantic-ui-react';
 
 export default function Test() {
