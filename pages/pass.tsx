95359a536e54a0e95340aaeac69d0f0a1d3e0039
Jbertalli <jjbertalli@berkeley.edu>
2023-01-12 19:33:25 -0800

    OpenNameDate component

--- pages/pass.tsx
+++ pages/pass.tsx
@@ -20,6 +20,7 @@ import Email from '../components/test_generator/Email';
 import CreateTimer from '../components/test_generator/CreateTimer';
 import DateClicked from '../components/test_generator/DateClicked';
 import NameClicked from '../components/test_generator/NameClicked';
+import OpenNameDate from '../components/test_generator/OpenNameDate';
 
 const LOCAL_STORAGE_KEY = 'list';
 const LOCAL_STORAGE_KEY_NAME = 'Name';
@@ -1028,7 +1029,8 @@ export default function Services() {
                         <div>
                             {!openNameDate ? (
                             <>
-                                <div
+                                <OpenNameDate setOpenNameDate={setOpenNameDate} questionColor={questionColor} />
+                                {/* <div
                                     style={{ 
                                         transform: 'translateY(-8px)',
                                         cursor: 'pointer' 
@@ -1058,116 +1060,11 @@ export default function Services() {
                                     >
                                         Open Name and Date
                                     </div>
-                                </div>
+                                </div> */}
                             </>
                             ):(
                             <>
-                                {/* <Container
-                                    style={{ 
-                                        color: 'red',
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                        cursor: 'pointer',
-                                        marginRight: '25px',
-                                        transform: 'translate(-1vw, -5px)'
-                                    }}
-                                    className={styles.open}
-                                    onClick={() => setOpenNameDate(false)}
-                                >
-                                    <div
-                                        style={{
-                                            transform: 'scale(2)',
-                                            zIndex: '10'
-                                        }}
-                                    >
-                                        x
-                                    </div>
-                                </Container> */}
                                 <NameClicked nameClicked={nameClicked} setName={setName} name={name} setNameClicked={setNameClicked} questionColor={questionColor} setOpenNameDate={setOpenNameDate} />
-                                {/* {!nameClicked ? (
-                                <>
-                                    <div>
-                                        <h2 
-                                            style={{ 
-                                                marginBottom: '5px' 
-                                            }}
-                                        >
-                                            Name
-                                        </h2>
-                                        <div
-                                            style={{
-                                                color: 'black'
-                                            }}
-                                        >
-                                            <input 
-                                                placeholder="Name"
-                                                style={{ 
-                                                    padding: '9px 14px 9px 14px',
-                                                    fontSize: '14px',
-                                                    fontWeight: '400',
-                                                    cursor: 'text',
-                                                    width: '178.5px',
-                                                    borderRadius: '4px',
-                                                    border: '1px solid rgba(34, 36, 38. 0.15)',
-                                                    position: 'relative',
-                                                    zIndex: '100'
-                                                }}
-                                                onChange={(e) => setName(e.target.value)}
-                                            />
-                                        </div>
-                                        {(name.length > 0) ? (
-                                        <>
-                                            <span 
-                                                style={{ 
-                                                    display: 'flex', 
-                                                    justifyContent: 'flex-end', 
-                                                    transform: 'translate(40px, -44px) scale(0.8)' 
-                                                }}
-                                            >
-                                                <Button 
-                                                    onClick= {() => setNameClicked(true)}
-                                                    style={{
-                                                        border: `2px solid ${questionColor}`,
-                                                        background: 'transparent',
-                                                        color: `${questionColor}`
-                                                    }}
-                                                >
-                                                    Save
-                                                </Button>
-                                            </span>
-                                        </>
-                                        ): null}
-                                    </div>
-                                </>
-                                ):(
-                                <>
-                                    <div style={{ transform: 'translateY(25px)' }}>
-                                        <span style={{ fontSize: '24px' }}>
-                                            <span style={{ fontWeight: '500' }}>
-                                                Name:{' '} 
-                                            </span>
-                                            <span style={{ fontWeight: '300' }}>
-                                                {name}
-                                            </span>
-                                        </span>
-                                        <span 
-                                            style={{ 
-                                                display: 'flex', 
-                                                justifyContent: 'flex-end', 
-                                                transform: 'translate(40px, -32px) scale(0.8)' 
-                                            }}
-                                        >
-                                            <Button 
-                                                color="blue"
-                                                onClick={() => {setName(''), setNameClicked(false)}}
-                                            >
-                                                Edit Name
-                                            </Button>
-                                        </span>
-                                    </div>
-                                </>
-                                )} */}
-
                                 <DateClicked dateClicked={dateClicked} setDate={setDate} setDateClicked={setDateClicked} questionColor={questionColor} date={date} />
                             </>
                             )}
