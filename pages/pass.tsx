3ec3c5d9de6c46e59c6dc9b35bdd888cbcbfd705
Jbertalli <jjbertalli@berkeley.edu>
2023-01-12 19:07:47 -0800

    CreateTimer component

--- pages/pass.tsx
+++ pages/pass.tsx
@@ -15,8 +15,9 @@ import MappedForm from '../components/test_generator/MappedForm';
 import GradeCard from '../components/test_generator/GradeCard';
 import EditTitle from '../components/test_generator/EditTitle';
 import DropdownKeys from '../components/test_generator/DropdownKeys';
-import TimerUI from '../components/test_generator/TimerUI';
+// import TimerUI from '../components/test_generator/TimerUI';
 import Email from '../components/test_generator/Email';
+import CreateTimer from '../components/test_generator/CreateTimer';
 
 const LOCAL_STORAGE_KEY = 'list';
 const LOCAL_STORAGE_KEY_NAME = 'Name';
@@ -942,7 +943,7 @@ export default function Services() {
                                     <>
                                         <div>
                                             <Button
-                                                onClick={() => {setStudent(false), setFinish(false), setIsTimed(false), setOpenAnswerKey(false), setOpenQuestionKey(false), setOpenStudentAnswers(false), setEdit(false), setSave(false)}}
+                                                onClick={() => {setStudent(false), setFinish(false), setIsTimed(false), setOpenAnswerKey(false), setOpenQuestionKey(false), setOpenStudentAnswers(false), setEdit(false), setSave(false), setTimeClick(false), setIsTimed(false)}}
                                                 style={{
                                                     border: `2px solid ${textColor}`,
                                                     background: 'transparent',
@@ -1034,139 +1035,6 @@ export default function Services() {
                     {student ? (
                     <>
                         <Email openEmail={openEmail} setOpenEmail={setOpenEmail} questionColor={questionColor} finish={finish} userEmail={userEmail} setUserEmail={setUserEmail} validEmail={validEmail} isValid={isValid} send={send} />
-                        {/* <Divider />
-                        <div>
-                            {!openEmail ? (
-                            <>
-                                <div
-                                    style={{ 
-                                        transform: 'translateY(-8px)',
-                                        cursor: 'pointer' 
-                                    }}
-                                    onClick={() => {setOpenEmail(true)}}
-                                >
-                                    <div 
-                                        style={{ 
-                                            marginLeft: '-25px',
-                                            display: 'flex',
-                                            color: `${questionColor}`,
-                                            transform: 'translateY(100%) scale(0.8)'
-                                        }}
-                                    >
-                                        <Icon
-                                            name='chevron down'
-                                        />
-                                    </div>
-                                    <div
-                                        style={{ 
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            fontSize: '20px', 
-                                            fontWeight: '700',
-                                            color: `${questionColor}`
-                                        }}
-                                    >
-                                        Email Grade
-                                    </div>
-                                </div>
-                            </>
-                            ):(
-                            <>
-                                <Container
-                                    style={{ 
-                                        color: 'red',
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                        cursor: 'pointer',
-                                        marginRight: '25px',
-                                        transform: 'translate(-1vw, -5px)'
-                                    }}
-                                    className={styles.open}
-                                    onClick={() => setOpenEmail(false)}
-                                >
-                                    <div
-                                        style={{
-                                            transform: 'scale(2)',
-                                            zIndex: '10'
-                                        }}
-                                    >
-                                        x
-                                    </div>
-                                </Container>
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        marginTop: '0px'
-                                    }}
-                                >
-                                    <div>
-                                        <h2>
-                                            Email Recipient
-                                        </h2>
-                                        <div
-                                            style={{
-                                                color: 'black'
-                                            }}
-                                        >
-                                            <input
-                                                disabled={finish ? false : true}
-                                                type='email'
-                                                name='user_email'
-                                                placeholder='email address'
-                                                style={{ 
-                                                    padding: '9px 14px 9px 14px',
-                                                    fontSize: '14px',
-                                                    fontWeight: '400',
-                                                    cursor: 'text',
-                                                    width: '178.5px',
-                                                    borderRadius: '4px',
-                                                    border: '1px solid rgba(34, 36, 38. 0.15)'
-                                                }}
-                                                value={userEmail}
-                                                onChange={(e) => {setUserEmail(e.target.value), validEmail(userEmail)}}
-                                            />
-                                        </div>
-                                        {(userEmail.length > 0 && !isValid) ? (
-                                        <>
-                                            <div
-                                                style={{
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    transform: 'translate(-5px, 50%)',
-                                                    color: 'red',
-                                                    fontSize: '20px',
-                                                    fontWeight: '500'
-                                                }}
-                                            >
-                                                Error: Email is invalid!
-                                            </div>
-                                        </>
-                                        ): null}
-                                        <div
-                                            style={{
-                                                display: 'flex',
-                                                justifyContent: 'center',
-                                                marginTop: '25px'
-                                            }}
-                                        >
-                                            <Button
-                                                disabled={!isValid} 
-                                                onClick={send}
-                                                style={{
-                                                    border: '2px solid #21BA45',
-                                                    background: 'transparent',
-                                                    color: '#21BA45'
-                                                }}
-                                            >
-                                                Email Grade
-                                            </Button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </>
-                            )}
-                        </div> */}
                         <Divider />
                         <div>
                             {!openNameDate ? (
@@ -1401,7 +1269,8 @@ export default function Services() {
                         </div>
                     </>
                     ): null}
-                    {!student ? (
+                    <CreateTimer student={student} edit={edit} setEdit={setEdit} questionColor={questionColor} setTimeClick={setTimeClick} setIsTimed={setIsTimed} timeClick={timeClick} timed={timed} minute={minute} setMinute={setMinute} second={second} setSecond={setSecond} />
+                    {/* {!student ? (
                     <>
                         <div>
                             <Divider />
@@ -1466,7 +1335,8 @@ export default function Services() {
                             )}
                         </div>
                     </>
-                    ): null}
+                    ): null} */}
+
                     <div>
                         <MyTimer isTimed={isTimed} setIsTimed={setIsTimed} setTimed={setTimed} timed={timed} student={student} finish={finish} setFinish={setFinish} secondTime={secondTime} minuteTime={minuteTime} expiryTimestamp={time} timeClick={timeClick} edit={edit} questionColor={questionColor} setTimeClick={setTimeClick} />
                     </div>
