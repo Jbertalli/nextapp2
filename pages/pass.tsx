3d7b516144a0c251b70e90a636c4a7c5a0299811
Jbertalli <jjbertalli@berkeley.edu>
2023-01-12 18:42:54 -0800

    Email component

--- pages/pass.tsx
+++ pages/pass.tsx
@@ -16,6 +16,7 @@ import GradeCard from '../components/test_generator/GradeCard';
 import EditTitle from '../components/test_generator/EditTitle';
 import DropdownKeys from '../components/test_generator/DropdownKeys';
 import TimerUI from '../components/test_generator/TimerUI';
+import Email from '../components/test_generator/Email';
 
 const LOCAL_STORAGE_KEY = 'list';
 const LOCAL_STORAGE_KEY_NAME = 'Name';
@@ -1032,7 +1033,8 @@ export default function Services() {
                     ): null}
                     {student ? (
                     <>
-                        <Divider />
+                        <Email openEmail={openEmail} setOpenEmail={setOpenEmail} questionColor={questionColor} finish={finish} userEmail={userEmail} setUserEmail={setUserEmail} validEmail={validEmail} isValid={isValid} send={send} />
+                        {/* <Divider />
                         <div>
                             {!openEmail ? (
                             <>
@@ -1164,7 +1166,7 @@ export default function Services() {
                                 </div>
                             </>
                             )}
-                        </div>
+                        </div> */}
                         <Divider />
                         <div>
                             {!openNameDate ? (
@@ -1460,132 +1462,6 @@ export default function Services() {
                                     </div>
                                 </Container>
                                 <TimerUI timeClick={timeClick} timed={timed} minute={minute} setMinute={setMinute} second={second} setSecond={setSecond} setTimeClick={setTimeClick} questionColor={questionColor} />
-                                {/* <div>
-                                    {timeClick ? (
-                                    <>
-                                        <div
-                                            style={{
-                                                display: 'flex',
-                                                justifyContent: 'center',
-                                                transform: 'translateY(-20px)'
-                                            }}
-                                        >
-                                            {!timed ? (
-                                            <>
-                                                <div
-                                                    style={{
-                                                        transform: 'translateY(-10px)'
-                                                    }}
-                                                >
-                                                    <h2
-                                                        style={{
-                                                            marginBottom: '5px',
-                                                            marginTop: '40px'
-                                                        }}
-                                                    >
-                                                        Minutes&nbsp;&nbsp;&nbsp;
-                                                    </h2>
-                                                    <div
-                                                        style={{
-                                                            display: 'flex',
-                                                            justifyContent: 'center',
-                                                            color: 'black'
-                                                        }}
-                                                    >
-                                                        <input
-                                                            placeholder='Minutes'
-                                                            type='number'
-                                                            min='0'
-                                                            max='59'
-                                                            value={minute}
-                                                            onChange={(e) => setMinute(e.target.value)}
-                                                            style={{
-                                                                padding: '9px 14px 9px 14px',
-                                                                fontSize: '14px',
-                                                                fontWeight: '400',
-                                                                cursor: 'text',
-                                                                width: '80px',
-                                                                borderRadius: '4px',
-                                                                border: '1px solid rgba(34, 36, 38. 0.15)',
-                                                                position: 'relative',
-                                                                zIndex: '100',
-                                                            }}
-                                                        />
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    style={{
-                                                        transform: 'translateY(-10px)'
-                                                    }}
-                                                >
-                                                    <h2
-                                                        style={{
-                                                            marginBottom: '5px',
-                                                            marginTop: '40px'
-                                                        }}
-                                                    >
-                                                        Seconds
-                                                    </h2>
-                                                    <div
-                                                        style={{
-                                                            display: 'flex',
-                                                            justifyContent: 'center',
-                                                            color: 'black'
-                                                        }}
-                                                    >
-                                                        <input
-                                                            placeholder='Seconds'
-                                                            type='number'
-                                                            min='0'
-                                                            max='59'
-                                                            value={second}
-                                                            onChange={(e) => setSecond(e.target.value)}
-                                                            style={{
-                                                                padding: '9px 14px 9px 14px',
-                                                                fontSize: '14px',
-                                                                fontWeight: '400',
-                                                                cursor: 'text',
-                                                                width: '80px',
-                                                                borderRadius: '4px',
-                                                                border: '1px solid rgba(34, 36, 38. 0.15)',
-                                                                position: 'relative',
-                                                                zIndex: '100',
-                                                            }}
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </>
-                                            ): null}
-                                        </div>
-                                    </>
-                                    ):(
-                                    <>
-                                        {!timed ? (
-                                        <>
-                                            <div
-                                                style={{
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    marginTop: '0px',
-                                                    marginBottom: '30px'
-                                                }}
-                                            >
-                                                <Button
-                                                    onClick={() => setTimeClick(true)}
-                                                    style={{
-                                                        border: `2px solid ${questionColor}`,
-                                                        background: 'transparent',
-                                                        color: `${questionColor}`
-                                                    }}
-                                                >
-                                                    Set Timer (optional)
-                                                </Button>
-                                            </div>
-                                        </>
-                                        ): null}
-                                    </>
-                                    )}
-                                </div> */}
                             </>
                             )}
                         </div>
