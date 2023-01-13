922d82bdca8511ff53899422fd539a852e88ee34
Jbertalli <jjbertalli@berkeley.edu>
2023-01-12 19:28:30 -0800

    NameClicked component

--- pages/pass.tsx
+++ pages/pass.tsx
@@ -19,6 +19,7 @@ import DropdownKeys from '../components/test_generator/DropdownKeys';
 import Email from '../components/test_generator/Email';
 import CreateTimer from '../components/test_generator/CreateTimer';
 import DateClicked from '../components/test_generator/DateClicked';
+import NameClicked from '../components/test_generator/NameClicked';
 
 const LOCAL_STORAGE_KEY = 'list';
 const LOCAL_STORAGE_KEY_NAME = 'Name';
@@ -1061,7 +1062,7 @@ export default function Services() {
                             </>
                             ):(
                             <>
-                                <Container
+                                {/* <Container
                                     style={{ 
                                         color: 'red',
                                         display: 'flex',
@@ -1081,8 +1082,9 @@ export default function Services() {
                                     >
                                         x
                                     </div>
-                                </Container>
-                                {!nameClicked ? (
+                                </Container> */}
+                                <NameClicked nameClicked={nameClicked} setName={setName} name={name} setNameClicked={setNameClicked} questionColor={questionColor} setOpenNameDate={setOpenNameDate} />
+                                {/* {!nameClicked ? (
                                 <>
                                     <div>
                                         <h2 
@@ -1164,95 +1166,9 @@ export default function Services() {
                                         </span>
                                     </div>
                                 </>
-                                )}
-                                <DateClicked dateClicked={dateClicked} setDate={setDate} setDateClicked={setDateClicked} questionColor={questionColor} date={date} />
-                                {/* {!dateClicked ? (
-                                <>
-                                    <div>
-                                        <h2 
-                                            style={{ 
-                                                marginBottom: '5px', 
-                                                marginTop: '20px' 
-                                            }}
-                                        >
-                                            Date
-                                        </h2>
-                                        <div
-                                            style={{
-                                                color: 'black'
-                                            }}
-                                        >
-                                            <input 
-                                                type="date"
-                                                placeholder="Date"
-                                                style={{ 
-                                                    padding: '9px 14px 9px 14px',
-                                                    fontSize: '14px',
-                                                    fontWeight: '400',
-                                                    cursor: 'text',
-                                                    width: '178.5px',
-                                                    borderRadius: '4px',
-                                                    border: '1px solid rgba(34, 36, 38. 0.15)',
-                                                    position: 'relative',
-                                                    zIndex: '100',
-                                                    marginBottom: '10px'
-                                                }}
-                                                onChange={(e) => setDate(e.target.value)}
-                                            />
-                                        </div>
-                                        {(date.length > 0) ? (
-                                        <>
-                                            <span 
-                                                style={{ 
-                                                    display: 'flex', 
-                                                    justifyContent: 'flex-end', 
-                                                    transform: 'translate(40px, -57px) scale(0.8)' 
-                                                }}
-                                            >
-                                                <Button 
-                                                    color="blue"
-                                                    onClick={() => setDateClicked(true)}
-                                                    style={{
-                                                        border: `2px solid ${questionColor}`,
-                                                        background: 'transparent',
-                                                        color: `${questionColor}`
-                                                    }}
-                                                >
-                                                    Save
-                                                </Button>
-                                            </span>
-                                        </>
-                                        ):null}
-                                    </div>
-                                </>
-                                ):(
-                                <>
-                                    <div style={{ transform: 'translateY(20px)' }}>
-                                        <span style={{ fontSize: '24px' }}>
-                                            <span style={{ fontWeight: '500' }}>
-                                                Date:{' '}
-                                            </span>
-                                            <span style={{ fontWeight: '300' }}>
-                                                {date}
-                                            </span>
-                                        </span>
-                                        <span 
-                                            style={{ 
-                                                display: 'flex', 
-                                                justifyContent: 'flex-end', 
-                                                transform: 'translate(40px, -32px) scale(0.8)' 
-                                            }}
-                                        >
-                                            <Button 
-                                                color="blue"
-                                                onClick={() => {setDate(''), setDateClicked(false)}}
-                                            >
-                                                Edit Date
-                                            </Button>
-                                        </span>
-                                    </div>
-                                </>
                                 )} */}
+
+                                <DateClicked dateClicked={dateClicked} setDate={setDate} setDateClicked={setDateClicked} questionColor={questionColor} date={date} />
                             </>
                             )}
                         </div>
